a8c1b4dedd335cb3b51038b2793c2fb67eb632b9
eduard.panasiuk <eduard.panasiuk@apalon.com>
2017-10-16 16:09:59 +0300

    Initial Commit

---
+++ TestClarifaiSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestClarifaiSDK
+//
+//  Created by Eduard Panasiuk on 10/16/17.
+//  Copyright © 2017 Somedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestClarifaiSDK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestClarifaiSDK
+//
+//  Created by Eduard Panasiuk on 10/16/17.
+//  Copyright © 2017 Somedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
